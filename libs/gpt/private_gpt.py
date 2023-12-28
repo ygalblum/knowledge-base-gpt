5796466ebd66e692553b225c3c4bb41e4d6bf362
Ygal Blum <ygal.blum@gmail.com>
2023-12-28 20:48:59 +0200

    Move PrivateGPT class to its own module
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

---
+++ libs/gpt/private_gpt.py
@@ -0,0 +1,60 @@
+import os
+
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.llms import Ollama
+from langchain.prompts import PromptTemplate
+from langchain.vectorstores import Chroma
+
+model = os.environ.get("MODEL", "llama2-uncensored")
+# For embeddings model, the example uses a sentence-transformers model
+# https://www.sbert.net/docs/pretrained_models.html
+# "The all-mpnet-base-v2 model provides the best quality, while all-MiniLM-L6-v2 is 5 times faster and still offers good quality."
+embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME", "all-MiniLM-L6-v2")
+persist_directory = os.environ.get("PERSIST_DIRECTORY", "db")
+target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
+
+better_prompt_template = """
+Use the following pieces of context to answer the question at the end. You can do two things:
+ 1. Return the answer to a question ONLY if the context contains the answer to the question.
+ 2. Return ONLY the text 'I dont know' if the context does not contain the answer to a question.
+
+{context}
+
+Question: {question}
+Helpful Answer:
+"""
+
+PROMPT = PromptTemplate(
+    template=better_prompt_template, input_variables=["context", "question"]
+)
+
+ollama_host = os.environ.get("OLLAMA_HOST", 'localhost')
+
+
+class PrivateGPT():
+    __instance = None
+
+    @staticmethod
+    def get_instance():
+        if PrivateGPT.__instance == None:
+            PrivateGPT()
+        return PrivateGPT.__instance
+
+    def __init__(self):
+        if PrivateGPT.__instance != None:
+            raise Exception("This class is a singleton!")
+        PrivateGPT.__instance = self
+
+    def initialize(self, hide_source):
+        self._hide_source = hide_source
+        embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+        db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+        retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+        llm = Ollama(model=model, base_url=f"http://{ollama_host}:11434")
+        self._qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=not self._hide_source, chain_type_kwargs=dict(prompt=PROMPT, verbose=True))
+
+    def answer_query(self, query):
+        res = self._qa(query)
+        answer, docs = res['result'], [] if self._hide_source else res['source_documents']
+        return answer
--- privateGPT.py
+++ privateGPT.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 import os
 
-from langchain.chains import RetrievalQA
-from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import Ollama
-from langchain.prompts import PromptTemplate
-from langchain.vectorstores import Chroma
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+from libs.gpt.private_gpt import PrivateGPT
 
 
 app = App(
@@ -15,68 +12,16 @@ app = App(
 )
 
 
-model = os.environ.get("MODEL", "llama2-uncensored")
-# For embeddings model, the example uses a sentence-transformers model
-# https://www.sbert.net/docs/pretrained_models.html
-# "The all-mpnet-base-v2 model provides the best quality, while all-MiniLM-L6-v2 is 5 times faster and still offers good quality."
-embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME", "all-MiniLM-L6-v2")
-persist_directory = os.environ.get("PERSIST_DIRECTORY", "db")
-target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
-
-better_prompt_template = """
-Use the following pieces of context to answer the question at the end. You can do two things:
- 1. Return the answer to a question ONLY if the context contains the answer to the question.
- 2. Return ONLY the text 'I dont know' if the context does not contain the answer to a question.
-
-{context}
-
-Question: {question}
-Helpful Answer:
-"""
-
-PROMPT = PromptTemplate(
-    template=better_prompt_template, input_variables=["context", "question"]
-)
-
-ollama_host = os.environ.get("OLLAMA_HOST", 'localhost')
-
-class PrivateGPT():
-    __instance = None
-
-    @staticmethod
-    def getInstance():
-        if PrivateGPT.__instance == None:
-            PrivateGPT()
-        return PrivateGPT.__instance
-
-    def __init__(self):
-        if PrivateGPT.__instance != None:
-            raise Exception("This class is a singleton!")
-        PrivateGPT.__instance = self
-
-    def initialize(self, hide_source):
-        self._hide_source = hide_source
-        embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
-        db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
-        retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-        llm = Ollama(model=model, base_url=f"http://{ollama_host}:11434")
-        self._qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=not self._hide_source, chain_type_kwargs=dict(prompt=PROMPT, verbose=True))
-
-    def answer_query(self, query):
-        res = self._qa(query)
-        answer, docs = res['result'], [] if self._hide_source else res['source_documents']
-        return answer
-
-
 @app.message()
 def got_message(message, say):
-    answer = PrivateGPT.getInstance().answer_query(message['text'])
+    answer = PrivateGPT.get_instance().answer_query(message['text'])
     say(answer)
 
 
 def main():
-    PrivateGPT.getInstance().initialize(True)
+    PrivateGPT.get_instance().initialize(True)
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+
 
 if __name__ == "__main__":
     main()
