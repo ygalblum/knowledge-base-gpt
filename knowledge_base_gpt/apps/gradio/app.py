a1084aa507d3812d6be084cb6b2b42a30f8fe4a1
Ygal Blum <ygal.blum@gmail.com>
2024-01-22 16:41:16 +0200

    Add button for forwarding conversations
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

--- knowledge_base_gpt/apps/gradio/app.py
+++ knowledge_base_gpt/apps/gradio/app.py
@@ -1,9 +1,12 @@
 #!/bin/env python3
+import logging
 
 import gradio as gr
 from langchain.schema import AIMessage, HumanMessage
 
 from knowledge_base_gpt.libs.gpt.private_chat import PrivateChat
+
+logger = logging.getLogger(__name__)
 
 class KnowledgeBaseGradioException(Exception):
     pass
@@ -25,12 +28,42 @@ class KnowledgeBaseGradio():
 
     def run(self):
         self._private_chat = PrivateChat()
+        chatbot=gr.Chatbot(
+            label=f"LLM: Ollama",
+            show_copy_button=True,
+            elem_id="chatbot",
+            render=False,
+        )
         with gr.Blocks() as demo:
-            chatbot = gr.Chatbot()
-            msg = gr.Textbox(label="", autofocus=True)
-            clear = gr.ClearButton([msg, chatbot])
-            msg.submit(KnowledgeBaseGradio.handle_query, [msg, chatbot], [msg, chatbot])
+            with gr.Row(equal_height=False):
+                with gr.Column(scale=7, elem_id="col"):
+                    self._chat_interface = gr.ChatInterface(
+                        self._handle_query,
+                        chatbot=chatbot
+                    )
+            with gr.Row(equal_height=False):
+                with gr.Column(scale=3):
+                    ingest_button = gr.Button("Forward your question")
+                    ingest_button.click(
+                        self._forward_question,
+                        inputs=chatbot
+                    )
         demo.launch()
+
+    @staticmethod
+    def _messages_to_text(messages):
+        text = ''
+        for human, bot in messages:
+            text += "Question: "
+            text += human
+            text += '\n'
+            text += "Answer: "
+            text += bot
+            text += '\n'
+        return text
+
+    def _forward_question(self, chatbot):
+        logger.error(self._messages_to_text(chatbot))
 
     def _handle_query(self, message, history):
         history_langchain_format = []
@@ -38,12 +71,7 @@ class KnowledgeBaseGradio():
             history_langchain_format.append(HumanMessage(content=human))
             history_langchain_format.append(AIMessage(content=ai))
         answer = self._private_chat.answer_query(history_langchain_format, message)
-        history.append((message, answer['answer']))
-        return "", history
-
-    @staticmethod
-    def handle_query(message, history):
-        return KnowledgeBaseGradio.get_instance()._handle_query(message, history)
+        return answer['answer']
 
 
 def main():
