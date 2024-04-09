1cd78e7e3ebac4b2b1922dc0c559a699350e08a7
Ygal Blum <ygal.blum@gmail.com>
2024-04-09 14:17:05 -0400

    TF for AWS - use module
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

--- cloud/terraform/aws/outputs.tf
+++ cloud/terraform/aws/outputs.tf
@@ -1,4 +1,4 @@
 output "external_services_exteranl_dns" {
-  description = "External DNS of the Ollama instalce"
-  value = aws_instance.ollama.public_dns
+  description = "External DNS of the Ollama instance"
+  value = module.ollama.external_services_exteranl_dns
 }
--- cloud/terraform/aws/variables.tf
+++ cloud/terraform/aws/variables.tf
@@ -1,29 +1,18 @@
-variable "key_name" {
+variable "ssh_key_name" {
   type = string
 }
 
-variable "ollama_instance_type" {
+variable "instance_name" {
     type = string
-    default = "g4dn.xlarge"
+    default = "llm"
 }
 
-variable "ollama_volume_size" {
+variable "instance_type" {
+    type = string
+    default = "g5.xlarge"
+}
+
+variable "volume_size" {
     type = number
-    default = 30
-
-}
-
-variable "ssh_port" {
-    type = number
-    default = 22
-}
-
-variable "ollama_port" {
-    type = number
-    default = 11434
-}
-
-variable "expose_webui" {
-    type = bool
-    default = false
+    default = 50
 }
