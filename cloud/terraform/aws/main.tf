9a967e790b5d5a9cd9e9be455615987d7b12dec8
Ygal Blum <ygal.blum@gmail.com>
2024-04-09 14:17:05 -0400

    TF for AWS - use module
    Signed-off-by: Ygal Blum <ygal.blum@gmail.com>

--- cloud/terraform/aws/main.tf
+++ cloud/terraform/aws/main.tf
@@ -1,49 +1,9 @@
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com"
-}
+module "ollama" {
+    source = "git@github.com:ygalblum/terraform-module-aws-instance.git"
 
-data "aws_vpc" "current" {
-  default = true
-}
-
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-locals {
-  myip_cidr = "${chomp(data.http.myip.response_body)}/32"
-  vpc_cidr_block = "${data.aws_vpc.current.cidr_block}"
-  security_group_ids = concat(
-    concat(
-      [aws_security_group.ssh.id, aws_security_group.outbound.id],
-      var.expose_webui ? [aws_security_group.webui[0].id] : []
-    ),
-    var.ollama_port == 0 ? [] : [aws_security_group.ollama[0].id]
-  )
-}
-
-data "aws_ami" "latest_centos_ami" {
-  most_recent = true
-  owners = ["125523088429"]
-  filter {
-    name   = "name"
-    values = ["CentOS Stream 9 x86_64*"]
-  }
-}
-
-resource "aws_instance" "ollama" {
-    tags = {
-        Name = "Ygal-Ollama"
-    }
-
-    ami           = data.aws_ami.latest_centos_ami.arn
-    instance_type = var.ollama_instance_type
-
-    key_name = var.key_name
-
-    vpc_security_group_ids = tolist(local.security_group_ids)
-
-    root_block_device {
-      volume_size = var.ollama_volume_size
-    }
+    ssh_key_name = var.ssh_key_name
+    instance_name = var.instance_name
+    instance_type = var.instance_type
+    volume_size = var.volume_size
+    application_ports = [8443, 19901, 12021]
 }
